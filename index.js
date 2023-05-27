55082214c652e4fb2bdba49946b245f921e703e9
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-27 19:42:11 +0700

    try

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 
 
 app.get('/data', (req, res) => {
-    
+
 
     return res.status(200).json({
         title: "Express Testing",
@@ -58,9 +58,39 @@ app.post('/', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+
+// Connect to MongoDB
+mongoose
+    .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB connected");
+        // Start the server after successful MongoDB connection
+        startServer();
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1);
+    });
+
+// Start the server
+function startServer() {
+    //   const http = require('http');
+    //   const port = process.env.PORT || 3000;
+
+    //   const server = http.createServer((req, res) => {
+    //     res.statusCode = 200;
+    //     res.setHeader('Content-Type', 'text/plain');
+    //     res.end('Hello, World!');
+    //   });
+
+    //   server.listen(port, () => {
+    //     console.log(`Server is listening on port ${port}`);
+    //   });
+
+    app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+    });
+}
 
 
 // const server = http.createServer(app);
