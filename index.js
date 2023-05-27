b4936537014631ea7e57f581ea782c81c4b0c7c9
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-27 19:14:29 +0700

    try

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const mongoose = require('mongoose')
+const http = require('http')
+const dotenv = require('dotenv')
+const cookieParser = require('cookie-parser')
+dotenv.config()
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(cors())
 
@@ -21,6 +30,21 @@ app.post('/', (req, res) => {
     });
 })
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+// app.listen(3000, () => {
+//     console.log('Server listening on port 3000');
+// });
+
+
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+  console.log("Mongodb connected");
+  server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}).catch((err) => {
+  console.log({ err });
+  process.exit(1);
 });
