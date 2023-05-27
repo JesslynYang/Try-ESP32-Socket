1eacb5185867ea813873d13e9b183c4089df702e
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-27 19:40:47 +0700

    fix

--- index.js
+++ index.js
@@ -23,20 +23,20 @@ app.get('/', (req, res) => {
 })
 
 
-const ProductQC = mongoose.model(
-    "ProductQC",
-    mongoose.Schema({
-        rfid_key: {
-            type: String,
-        },
-        humidity: {
-            type: String,
-        },
-        temperature: {
-            type: String,
-        },
-    })
-);
+// const ProductQC = mongoose.model(
+//     "ProductQC",
+//     mongoose.Schema({
+//         rfid_key: {
+//             type: String,
+//         },
+//         humidity: {
+//             type: String,
+//         },
+//         temperature: {
+//             type: String,
+//         },
+//     })
+// );
 
 
 app.get('/data', (req, res) => {
@@ -65,12 +65,12 @@ app.listen(port, () => {
 
 // const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongodb connected");
-//   server.listen(port, () => {
-//     console.log(`Server is listening on port ${port}`);
-//   });
-}).catch((err) => {
-  console.log({ err });
-  process.exit(1);
-});
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//   console.log("Mongodb connected");
+// //   server.listen(port, () => {
+// //     console.log(`Server is listening on port ${port}`);
+// //   });
+// }).catch((err) => {
+//   console.log({ err });
+//   process.exit(1);
+// });
