3e5e58262a8acd8f834fa9300f0733721d634c76
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-27 19:05:59 +0700

    Fix

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const app = express();
-const http = require('http').createServer(app);
-const WebSocket = require('ws');
 const cors = require('cors')
-
-const wss = new WebSocket.Server({ server: http });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -25,26 +21,6 @@ app.post('/', (req, res) => {
     });
 })
 
-wss.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('message', (message) => {
-        console.log('Received message from client:', message);
-        // Perform any required actions based on the received message
-
-        // Example: Broadcast the message to all connected clients
-        wss.clients.forEach((client) => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-
-    socket.on('close', () => {
-        console.log('A user disconnected');
-    });
-});
-
-http.listen(3000, () => {
+app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
