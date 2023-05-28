d03e9311e70c035075846c75c20a125160042e0e
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-28 16:12:16 +0700

    fix

--- index.js
+++ index.js
@@ -5,26 +5,14 @@ const mongoose = require('mongoose')
 const http = require('http')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
-const connectToMongoDB = require('./middlewares/connectToMongoDB')
-const { connectToDatabase } = require('./config/dbConn')
+// const connectToMongoDB = require('./middlewares/connectToMongoDB')
+// const { connectToDatabase } = require('./config/dbConn')
 dotenv.config()
 
-// app.use(express.static(__dirname + '/public'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// mongoose.connect(process.env.MONGODB_URL).then(() => {
-//     //   console.log("Mongodb connected");
-//     //   server.listen(port, () => {
-//     //     console.log(`Server is listening on port ${port}`);
-//     //   });
-// }).catch((err) => {
-//     console.log({ err });
-//     process.exit(1);
-// });
-
 app.use(cors())
 
 const ProductQC = mongoose.model(
@@ -51,12 +39,9 @@ app.get('/', async (req, res) => {
     return res.status(200).json({
         title: "Express Testing",
         message: "The app is working properly!",
-        datas,
+        // datas,
     });
 })
-
-
-
 
 app.get('/data', (req, res) => {
     return res.status(200).json({
@@ -79,43 +64,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
-
-
-// Connect to MongoDB
-// mongoose
-//     .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log("MongoDB connected");
-//         // Start the server after successful MongoDB connection
-//         startServer();
-//     })
-//     .catch((err) => {
-//         console.error("Failed to connect to MongoDB", err);
-//         process.exit(1);
-//     });
-
-// // Start the server
-// function startServer() {
-//     //   const http = require('http');
-//     //   const port = process.env.PORT || 3000;
-
-//     //   const server = http.createServer((req, res) => {
-//     //     res.statusCode = 200;
-//     //     res.setHeader('Content-Type', 'text/plain');
-//     //     res.end('Hello, World!');
-//     //   });
-
-//     //   server.listen(port, () => {
-//     //     console.log(`Server is listening on port ${port}`);
-//     //   });
-
-//     app.listen(port, () => {
-//         console.log(`Server listening on port ${port}`);
-//     });
-// }
-
-
-// const server = http.createServer(app);
-
 
 
