542d7eb04ccdaa20d91de8e7fa5b59bef97ec005
Jesslyn Yang <jesslynyang@Jesslyn-Yang.local>
2023-05-28 15:53:27 +0700

    fix'

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-    //   console.log("Mongodb connected");
-    //   server.listen(port, () => {
-    //     console.log(`Server is listening on port ${port}`);
-    //   });
-}).catch((err) => {
-    console.log({ err });
-    process.exit(1);
-});
+// mongoose.connect(process.env.MONGODB_URL).then(() => {
+//     //   console.log("Mongodb connected");
+//     //   server.listen(port, () => {
+//     //     console.log(`Server is listening on port ${port}`);
+//     //   });
+// }).catch((err) => {
+//     console.log({ err });
+//     process.exit(1);
+// });
 
 app.use(cors())
 
